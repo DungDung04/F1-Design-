b7a46de24d1e6ea6fb6510ced2203bda5ddacd67
DungDung04 <nguyentrungdung5102004@gmail.com>
2026-07-09 21:20:04 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,37 +1,26 @@
-`default_nettype none
 `timescale 1ns/1ps
 
-module tb (
-    input wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input wire clk,
-    input wire rst_n,
-    input wire ena
-);
+module tb;
 
-    initial begin
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb);
-        #1;
-    end
+    reg clk;
+    reg rst_n;
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
+    reg ena;
 
-tt_um_nguyentrungdung04_dff_async user_project (
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-`ifdef GL_TEST
-        .VPWR(1'b1),
-        .VGND(1'b0),
-`endif
-        .ui_in   (ui_in),
-        .uo_out  (uo_out),
-        .uio_in  (uio_in),
-        .uio_out (uio_out),
-        .uio_oe  (uio_oe),
-        .ena     (ena),
-        .clk     (clk),
-        .rst_n   (rst_n)
+    project dut (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
     );
 
 endmodule
