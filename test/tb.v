ff839e57ee7c0262ee646eab968e744cbb90cc11
DungDung04 <nguyentrungdung5102004@gmail.com>
2026-07-13 22:19:33 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -2,25 +2,25 @@
 
 module tb;
 
-    reg clk;
-    reg rst_n;
-    reg [7:0] ui_in;
-    reg [7:0] uio_in;
-    reg ena;
+reg clk;
+reg rst_n;
+reg [7:0] ui_in;
+reg [7:0] uio_in;
+reg ena;
 
-    wire [7:0] uo_out;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
+wire [7:0] uo_out;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
 
-    tt_um_nguyentrungdung04_dff_async dut (
-        .ui_in(ui_in),
-        .uo_out(uo_out),
-        .uio_in(uio_in),
-        .uio_out(uio_out),
-        .uio_oe(uio_oe),
-        .ena(ena),
-        .clk(clk),
-        .rst_n(rst_n)
-    );
+tt_um_nguyentrungdung04_dff_async dut(
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+);
 
 endmodule
