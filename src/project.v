64fd1f28091a17736c2a75a70faff4de505b7881
DungDung04 <nguyentrungdung5102004@gmail.com>
2026-07-14 17:55:54 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,44 +12,43 @@ module tt_um_nguyentrungdung04_dff_async (
     input  wire rst_n
 );
 
-wire _unused = &{ena,uio_in,ui_in[7:1],1'b0};
+    // tránh warning unused
+    wire _unused = &{
+        ena,
+        uio_in,
+        ui_in[7:1],
+        1'b0
+    };
 
-wire d;
-wire q;
+    wire q;
 
-assign d = ui_in[0];
+    dff_async dff_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .d(ui_in[0]),
+        .q(q)
+    );
 
-dff_async u_dff(
-    .clk(clk),
-    .rst_n(rst_n),
-    .d(d),
-    .q(q)
-);
+    assign uo_out  = {7'b0000000, q};
 
-assign uo_out = {7'b0,q};
-
-assign uio_out = 8'b0;
-assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
 endmodule
 
 
 module dff_async(
-
-    input wire clk,
-    input wire rst_n,
-    input wire d,
-    output reg q
-
+    input  wire clk,
+    input  wire rst_n,
+    input  wire d,
+    output reg  q
 );
 
 always @(posedge clk or negedge rst_n) begin
-
-    if(!rst_n)
+    if (!rst_n)
         q <= 1'b0;
     else
         q <= d;
-
 end
 
 endmodule
